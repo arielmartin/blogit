6661474b2c274b62d70ec317b0efa03a92ff2386
Ariel Martin <arielmartin80@gmail.com>
2022-03-20 18:09:51 -0300

    se creo comando install

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Rol::class, 'usuarios_roles', 'usuarios_id', 'roles_id');
+    }
+
 }
